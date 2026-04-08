d49fb1e3db1faefbda7c733514f0d31f3978e900
Quavohunc <le.seigneur.gaara@gmail.com>
2026-04-08 09:53:09 +0000

    feat: ajouter affichage du nom

--- src/main/java/com/formation/App.java
+++ src/main/java/com/formation/App.java
@@ -7,5 +7,6 @@ public class App {
     public static void main(String[] args) {
         System.out.println("Hello World!");
         System.out.println("java version: " + System.getProperty("java.version"));
+        //System.out.println("Junior");
     }
 }
